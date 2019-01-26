f224a5e24480e355ab0899ffa3a5d84442ace09b
Matthew Dyet <matthew@diomades.com>
2019-01-26 14:32:47 +0800

    Planet Has Looping Continents and Clouds
    Planetary rotation visual now looks good!

--- Assets/Scripts/Game/WorldMover.cs
+++ Assets/Scripts/Game/WorldMover.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class WorldMover : MonoBehaviour
 {
     public GameObject worldMap;
+
     public GameObject continents;
     public GameObject continents2;
+
     public GameObject clouds;
     public GameObject clouds2;
 
@@ -14,8 +16,14 @@ public class WorldMover : MonoBehaviour
     public float cloudMoveSpeed;
 
     private bool _moving = false;
+
     private GameObject _curContinent;
+    private float _continentWidth;
+    private float _continentRespawnPosX;
+
     private GameObject _curClouds;
+    private float _cloudsWidth;
+    private float _cloudsRespawnPosX;
 
     public void StartMoving()
     {
@@ -38,25 +46,65 @@ public class WorldMover : MonoBehaviour
             continents2.transform.Translate(continentMoveSpeed * Time.deltaTime, 0, 0);
             clouds.transform.Translate(cloudMoveSpeed * Time.deltaTime, 0, 0);
             clouds2.transform.Translate(cloudMoveSpeed * Time.deltaTime, 0, 0);
+
+            //Check to see if we need to relocate anything
+            if (_curContinent.transform.position.x >= _continentRespawnPosX)
+            {
+                if (_curContinent.name == "WorldMap")
+                {
+                    Debug.Log("Moving WorldMapBig");
+                    _curContinent.transform.position = SpawnPosition(continents2.transform.position, _continentWidth);
+                    _curContinent = continents2;
+                }
+                else
+                {
+                    Debug.Log("Moving WorldMapBig(Clone)");
+                    _curContinent.transform.position = SpawnPosition(continents.transform.position, _continentWidth);
+                    _curContinent = continents;
+                }
+            }
+
+            if(_curClouds.transform.position.x >= _cloudsRespawnPosX)
+            {
+                if (_curClouds.name == "Clouds")
+                {
+                    Debug.Log("Moving Clouds");
+                    _curClouds.transform.position = SpawnPosition(clouds2.transform.position, _cloudsWidth);
+                    _curClouds = clouds2;
+                }
+                else
+                {
+                    Debug.Log("Moving Clouds(Clone)");
+                    _curClouds.transform.position = SpawnPosition(clouds.transform.position, _cloudsWidth);
+                    _curClouds = clouds;
+                }
+            }
         }
     }
 
     private void InstantiateContinent2()
     {
         BoxCollider2D curCollider = continents.transform.GetComponent<BoxCollider2D>(); //Get the box collider
+        _continentWidth = curCollider.size.x * continents.transform.localScale.x;
 
         //Instantiate new object at the current placement, and then relocate it appropriately
         continents2 = Instantiate(continents, worldMap.transform);
-        continents2.transform.position = SpawnPosition(continents.transform.position, (curCollider.size.x * continents.transform.localScale.x));
+        continents2.transform.position = SpawnPosition(continents.transform.position, _continentWidth);
+
+        //While we are here, let's also set the Continent respawn position
+        _continentRespawnPosX = continents.transform.position.x + _continentWidth;
     }
 
     private void InstantiateClouds2()
     {
         BoxCollider2D curCollider = clouds.transform.GetComponent<BoxCollider2D>(); //Get the box collider
+        _cloudsWidth = curCollider.size.x * clouds.transform.localScale.x;
 
         //Instantiate new object at the current placement, and then relocate it appropriately
         clouds2 = Instantiate(clouds, worldMap.transform);
-        clouds2.transform.position = SpawnPosition(clouds.transform.position, (curCollider.size.x * clouds.transform.localScale.x));
+        clouds2.transform.position = SpawnPosition(clouds.transform.position, _cloudsWidth);
+
+        _cloudsRespawnPosX = clouds.transform.position.x + _cloudsWidth;
     }
 
     //Takes a Vector3 position and Collider width to decide on a spawn position
@@ -67,6 +115,4 @@ public class WorldMover : MonoBehaviour
 
         return spawnPosition;
     }
-
-    //private void MoveObjectBehindCurrent
 }
